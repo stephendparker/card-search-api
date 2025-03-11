a35c5636640e77fd2584bdf8252c8d9d08f678ab
Stephen Parker <stephendparker@gmail.com>
2025-03-11 18:57:04 -0400

    add cors configurability

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  const allowedOrigins = [
+    configService.get<string>('FRONTEND_URL'),
+    configService.get<string>('PRODUCTION_FRONTEND_URL'),
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:4200', // ✅ Allow frontend
+    origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // ✅ Allow these HTTP methods
     credentials: true, // ✅ Allow cookies/auth headers if needed
   });
